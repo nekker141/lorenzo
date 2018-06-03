787effa3b93d1aa5274ad72a6d0193839d181e12
nekker141 <sergio141.96@gmail.com>
2018-06-03 23:27:00 +0200

    1

--- PaginaFinal 1.0/bd.sql
+++ PaginaFinal 1.0/bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-06-2018 a las 21:27:30
+-- Tiempo de generación: 03-06-2018 a las 23:26:17
 -- Versión del servidor: 10.1.32-MariaDB
 -- Versión de PHP: 7.2.5
 
@@ -21,6 +21,54 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `proyecto`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cita`
+--
+
+CREATE TABLE `cita` (
+  `id` int(20) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_general_mysql500_ci NOT NULL,
+  `apellidos` varchar(100) COLLATE utf8_general_mysql500_ci NOT NULL,
+  `provincia` varchar(50) COLLATE utf8_general_mysql500_ci NOT NULL,
+  `ciudad` varchar(30) COLLATE utf8_general_mysql500_ci NOT NULL,
+  `dia` date NOT NULL,
+  `hora` varchar(20) COLLATE utf8_general_mysql500_ci NOT NULL,
+  `cp` int(10) NOT NULL,
+  `motivos` varchar(5000) COLLATE utf8_general_mysql500_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+--
+-- Volcado de datos para la tabla `cita`
+--
+
+INSERT INTO `cita` (`id`, `nombre`, `apellidos`, `provincia`, `ciudad`, `dia`, `hora`, `cp`, `motivos`) VALUES
+(1, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(2, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(3, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(4, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(5, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(6, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(7, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(8, '', '0', '', '', '0000-00-00', '', 46017, ''),
+(9, 'Sergio', '0', '', '', '0000-00-00', '', 46017, ''),
+(10, 'Sergio', '0', 'Valencia', 'Valencia', '2018-06-22', '10', 46017, 'asdasd'),
+(11, 'Sergio', 'GuillÃ©n Coso', 'Valencia', 'Madrid', '2018-06-30', '12', 9999999, 'Pollon'),
+(12, 'Sergio', 'GuillÃ©n Coso', 'Valencia', 'Madrid', '2018-06-30', '12', 9999999, 'Pollon'),
+(13, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-21', '123', 46017, 'asdads'),
+(14, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-29', '45', 46017, 'asdaasd'),
+(15, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-29', '45', 46017, 'asdaasd'),
+(16, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-29', '20', 46017, 'asdaasd'),
+(17, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-20', '300', 46017, 'asddas'),
+(18, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-21', '12', 46017, 'asdads'),
+(19, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-21', '12', 46017, 'asd'),
+(20, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-21', '12', 46017, 'asdasd'),
+(21, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-14', '12', 46017, 'asdasdas'),
+(22, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-27', '12', 46017, 'asdas'),
+(23, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-21', '12', 46017, 'asdads'),
+(24, 'Sergio', 'GuillÃ©n', 'Valencia', 'Valencia', '2018-06-22', '12', 46017, 'asd');
 
 -- --------------------------------------------------------
 
@@ -50,7 +98,8 @@ INSERT INTO `comentarios` (`comentario`, `username`, `fecha`, `articulo`) VALUES
 ('pollon', '', '2018-06-03', 1),
 ('pollitafina', 'sergio', '2018-06-03', 1),
 ('jejej ', 'sergio', '2018-06-03', 2),
-('que loco xd', 'sergio', '2018-06-03', 2);
+('que loco xd', 'sergio', '2018-06-03', 2),
+('asdasd', 'sergio', '2018-06-03', 1);
 
 -- --------------------------------------------------------
 
@@ -81,6 +130,12 @@ INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
 --
 
 --
+-- Indices de la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
@@ -89,6 +144,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `cita`
+--
+ALTER TABLE `cita`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
